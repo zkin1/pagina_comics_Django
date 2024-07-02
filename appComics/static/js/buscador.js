1426c3e8d12e9346ff06c4e4880064c29a388307
niitcoh <netb.20133@gmail.com>
2024-07-02 00:09:19 -0400

    Ahora funciona el buscador , falta arreglar las cards

--- appComics/static/js/buscador.js
+++ appComics/static/js/buscador.js
@@ -1,122 +1,85 @@
-function filterComics(comics, searchTerm) {
-  return comics.filter(comic => {
-    const name = comic.nombre.toLowerCase();
-    const description = comic.descripcion.toLowerCase();
-    const term = searchTerm.toLowerCase();
-    return name.includes(term) || description.includes(term);
-  });
-}
-
+let getComicsUrl;
 let comics = []; // Declarar la variable 'comics' fuera de la función de renderizado
 
-// Función para renderizar los cómics
-function renderComics(comicData, filteredComics, searchTerm) {
-  const filteredComicsContainer = $('#filtered-comics-list');
-  const allComicsContainer = $('#all-comics-list');
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener la URL de la vista desde el meta tag
+    getComicsUrl = document.querySelector('meta[name="get-comics-url"]').getAttribute('content');
 
-  filteredComicsContainer.empty();
-  allComicsContainer.empty();
-
-  const comicsToRender = searchTerm ? filteredComics : comicData;
-
-  comicsToRender.forEach((comic, index) => {
-    const comicCard = `
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="http://localhost:3000${comic.foto}" class="card-img-top" alt="${comic.nombre}">
-          <div class="card-body">
-            <h5 class="card-title">${comic.nombre}</h5>
-            <p class="card-text">Precio: $${comic.precio}</p>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#comicModal${index}">Ver más</button>
-          </div>
-        </div>
-      </div>
-      <!-- Modal -->
-      <div class="modal fade" id="comicModal${index}" tabindex="-1" role="dialog" aria-labelledby="comicModalLabel${index}" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="comicModalLabel${index}">${comic.nombre}</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <div class="row">
-                <div class="col-md-4">
-                  <img src="http://localhost:3000${comic.foto}" class="img-fluid" alt="${comic.nombre}">
-                </div>
-                <div class="col-md-8">
-                  <p><strong>Descripción:</strong> ${comic.descripcion}</p>
-                  <p><strong>Precio:</strong> $${comic.precio}</p>
-                  <p><strong>Stock:</strong> ${comic.stock}</p>
-                  <button type="button" class="btn btn-success add-to-cart" data-name="${comic.nombre}">Agregar al carrito</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-
-    if (searchTerm) {
-      filteredComicsContainer.append(comicCard);
-    } else {
-      allComicsContainer.append(comicCard);
+    function filterComics(comics, searchTerm) {
+        return comics.filter(comic => {
+            const name = comic.nombre.toLowerCase();
+            const description = comic.descripcion.toLowerCase();
+            const term = searchTerm.toLowerCase();
+            return name.includes(term) || description.includes(term);
+        });
     }
-  });
 
-  comics = comicData; // Asignar los datos de los cómics a la variable 'comics'
-}
+    // Función para renderizar los cómics
+    function renderComics(comicData, filteredComics, searchTerm) {
+        const filteredComicsContainer = $('#filtered-comics-list');
+        const allComicsContainer = $('#all-comics-list');
 
-// Función para agregar un producto al carro
-function addToCart(comic) {
-  const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-  const existingItem = cartItems.find(item => item.nombre === comic.nombre);
+        filteredComicsContainer.empty();
+        allComicsContainer.empty();
 
-  if (existingItem) {
-    existingItem.quantity++;
-    existingItem.subtotal = existingItem.precio * existingItem.quantity;
-  } else {
-    const newItem = {
-      nombre: comic.nombre,
-      precio: comic.precio,
-      foto: comic.foto,
-      quantity: 1,
-      subtotal: comic.precio // Asegurarse de que subtotal sea un número
-    };
-    cartItems.push(newItem);
-  }
+        const comicsToRender = searchTerm ? filteredComics : comicData;
 
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        comicsToRender.forEach((comic, index) => {
+            const comicImageUrl = `/static${comic.foto}`;
 
-  // Mostrar el modal de confirmación
-  $('#addToCartModal').modal('show');
-}
+            const comicCard = `
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="${comicImageUrl}" class="card-img-top" alt="${comic.nombre}">
+                        <div class="card-body">
+                            <h5 class="card-title">${comic.nombre}</h5>
+                            <p class="card-text">Precio: $${comic.precio}</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#comicModal${index}">Ver más</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="comicModal${index}" tabindex="-1" role="dialog" aria-labelledby="comicModalLabel${index}" aria-hidden="true">
+                    <div class="modal-dialog="modal-header">
+                                <h5 class="modal-title" id="comicModalLabel${index}">${comic.nombre}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                               ="col-md-4">
+                                        <img src="${comicImageUrl}" class="img-fluid" alt="${comic.nombre}">
+                                    </="col-md-8">
+                                        <p><strong>Descripción:</strong> ${comic.descripcion}</p>
+                                        <p><strong>Precio:</strong> $${comic.precio}</p>
+                                        <p><strong>Stock:</strong> ${comic.stock}</p>
+                                        <button type="button" class="btn btn-success add-to-cart" data-name="${comic.nombre}">Agregar al carrito</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
 
-// Evento para agregar un producto al carro
-$('#all-comics-list, #filtered-comics-list').on('click', '.add-to-cart', function() {
-  const comicName = $(this).data('name');
-  if (comicName) {
-    const comic = comics.find(comic => comic.nombre === comicName);
-    if (comic) {
-      addToCart(comic);
-    } else {
-      console.error('Comic not found with name:', comicName);
+            if (searchTerm) {
+                filteredComicsContainer.append(comicCard);
+            } else {
+                allComicsContainer.append(comicCard);
+            }
+        });
+
+        comics = comicData; // Asignar los datos de los cómics a la variable 'comics'
     }
-  } else {
-    console.error('Invalid comic name');
-  }
-});
 
-// Agregar un event listener para el botón de búsqueda
-$('#search-btn').on('click', function() {
-  const searchTerm = $('#search-input').val().trim();
-  fetch('http://localhost:3000/api/comics')
-    .then(response => response.json())
-    .then(comics => {
-      const filteredComics = filterComics(comics, searchTerm);
-      renderComics(comics, filteredComics, searchTerm);
-    })
-    .catch(error => console.error('Error al obtener los cómics:', error));
+    // Función para agregar un producto al carro (igual que antes)
+    // ...
+
+    // Event listener para el botón de búsqueda
+    $('#search-btn').on('click', function () {
+        const searchTerm = $('#search-input').val().trim();
+        fetch(getComicsUrl) // Usar la URL obtenida del meta tag
+            .then(response => response.json())
+            .then(comics => {
+                const filteredComics = filterComics(comics, searchTerm);
+                renderComics(comics, filteredComics, searchTerm);
+            })
+            .catch(error => console.error('Error al obtener los cómics:', error));
+    });
 });
