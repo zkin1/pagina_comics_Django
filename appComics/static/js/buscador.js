ddda7bfd95bf00db2de7aa6f693e8dafddc0d9be
niitcoh <netb.20133@gmail.com>
2024-07-02 18:11:42 -0400

    Ya funcionan las cards en todos lados

--- appComics/static/js/buscador.js
+++ appComics/static/js/buscador.js
@@ -24,39 +24,50 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const comicsToRender = searchTerm ? filteredComics : comicData;
 
-        comicsToRender.forEach((comic, index) => {
+        comicData.forEach((comic, index) => {
+            // Ajustar la URL de la imagen
             const comicImageUrl = `/static${comic.foto}`;
-
+        
             const comicCard = `
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                        <img src="${comicImageUrl}" class="card-img-top" alt="${comic.nombre}">
-                        <div class="card-body">
-                            <h5 class="card-title">${comic.nombre}</h5>
-                            <p class="card-text">Precio: $${comic.precio}</p>
-                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#comicModal${index}">Ver más</button>
-                        </div>
-                    </div>
+              <div class="col-md-4 mb-4">
+                <div class="card">
+                  <img src="${comicImageUrl}" class="card-img-top" alt="${comic.nombre}">
+                  <div class="card-body">
+                    <h5 class="card-title">${comic.nombre}</h5>
+                    <p class="card-text">Precio: $${comic.precio}</p>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#comicModal${index}">Ver más</button>
+                  </div>
                 </div>
-                <div class="modal fade" id="comicModal${index}" tabindex="-1" role="dialog" aria-labelledby="comicModalLabel${index}" aria-hidden="true">
-                    <div class="modal-dialog="modal-header">
-                                <h5 class="modal-title" id="comicModalLabel${index}">${comic.nombre}</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                               ="col-md-4">
-                                        <img src="${comicImageUrl}" class="img-fluid" alt="${comic.nombre}">
-                                    </="col-md-8">
-                                        <p><strong>Descripción:</strong> ${comic.descripcion}</p>
-                                        <p><strong>Precio:</strong> $${comic.precio}</p>
-                                        <p><strong>Stock:</strong> ${comic.stock}</p>
-                                        <button type="button" class="btn btn-success add-to-cart" data-name="${comic.nombre}">Agregar al carrito</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+              </div>
+              <!-- Modal -->
+              <div class="modal fade" id="comicModal${index}" tabindex="-1" role="dialog" aria-labelledby="comicModalLabel${index}" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="comicModalLabel${index}">${comic.nombre}</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
                     </div>
+                    <div class="modal-body">
+                      <div class="row">
+                        <div class="col-md-4">
+                          <img src="${comicImageUrl}" class="img-fluid" alt="${comic.nombre}">
+                        </div>
+                        <div class="col-md-8">
+                          <p><strong>Descripción:</strong> ${comic.descripcion}</p>
+                          <p><strong>Precio:</strong> $${comic.precio}</p>
+                          <p><strong>Stock:</strong> ${comic.stock}</p>
+                          <button type="button" class="btn btn-success add-to-cart" data-name="${comic.nombre}">Agregar al carrito</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              </div>
             `;
+
+          comics = comicData;
 
             if (searchTerm) {
                 filteredComicsContainer.append(comicCard);
